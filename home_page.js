3ceb962d9cf3bd358a848a49f021b287fc36450e
ultimafinal1 <matthewblackman123@gmail.com>
2022-09-20 16:03:20 +0100

    cannot use dupe username

--- home_page.js
+++ home_page.js
@@ -3,16 +3,35 @@ const loginForm = document.querySelector("#loginForm");
 
 registerForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const options = {
-        method : "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-    }
-    console.log(options);
-    fetch("http://localhost:3000/users", options)
-    .then(res => {
-        console.log(res);
-        res.json();
-    })
-    .then(data => console.log(data));
+    checkUsernameExists(e);
 })
+
+async function checkUsernameExists(e) {
+    try {
+        await fetch(`http://localhost:3000/users`)
+        .then(res => res.json())
+        .then(data => {
+            let duplicate = 0;
+            data.forEach(user => {
+                if (e.target.name.value === user.name) {
+                    duplicate++;
+                }
+            })
+            if (duplicate === 0) {
+                e.preventDefault();
+                const options = {
+                    method : "POST",
+                    headers: { "Content-Type": "application/json"},
+                    body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+                }
+                fetch("http://localhost:3000/users", options);
+            }
+            else {
+                console.log('Username in use.');
+            }
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
