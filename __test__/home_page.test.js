9acd68f03199572583ea97a0d44e585ad299bfc5
ultimafinal1 <matthewblackman123@gmail.com>
2022-09-20 09:49:45 +0100

    client side for site

---
+++ __test__/home_page.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ const fs = require("fs");
+ const path = require("path");
+ const html = fs.readFileSync(path.resolve(__dirname, "../home_page.html"), "utf-8") 
+ 
+ describe("home page", () => {
+     beforeEach(() => {
+         document.documentElement.innerHTML = html.toString();  //beforeEach can be used if an action needs to be repeated before each test
+     })
+     it('h1 shows "Accomplish" when website loads', () => {
+         const h1 = document.querySelector('h1');
+         expect(h1.innerHTML).toBe("Accomplish")
+       })
+    })
