286d553c85e75c246edf922c6ea3c877ae05b698
ultimafinal1 <matthewblackman123@gmail.com>
2022-09-21 10:47:18 +0100

    work on login process

--- home_page.js
+++ home_page.js
@@ -1,14 +1,23 @@
 const registerForm = document.querySelector("#registerForm");
 const loginForm = document.querySelector("#loginForm");
 const closeButton1 = document.querySelector('#close1');
+const closeButton2 = document.querySelector('#close2');
 
 registerForm.addEventListener("submit", (e) => {
     e.preventDefault()
     checkUsernameExists(e)
 })
 
+loginForm.addEventListener("submit", (e) => {
+    accountLogin(e);
+})
+
 closeButton1.addEventListener("click", (e) => {
     resetRegistration();
+})
+
+closeButton2.addEventListener("click", (e) => {
+    resetLogin();   
 })
 
 async function checkUsernameExists(e) {
@@ -54,7 +63,30 @@ async function registerAccount(e) {
         alert('Passwords do not match');
     }
     document.querySelector('#registerForm').reset();
+}
 
+async function accountLogin(e) {
+    e.preventDefault();
+    await fetch("http://localhost:3000/users")
+    .then(res => res.json())
+    .then(data => {
+        let user = data.filter(user => e.target.name.value === user.name);
+        for (const userDetails of user) {
+            console.log(userDetails);
+            if (e.target.password.value === userDetails.password) {
+                window.location.href = 'user_home_page.html';
+                resetLogin();
+            }
+            else {
+                alert('Login failed.');
+                resetLogin();
+            }
+        } 
+    })
+}
+
+function resetLogin() {
+    document.querySelector('#loginForm').reset();
 }
 
 function resetRegistration() {
