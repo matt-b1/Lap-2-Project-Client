13d81bd5c8207caf0be6183f4a27b7b5ec3c0290
ultimafinal1 <matthewblackman123@gmail.com>
2022-09-21 10:03:33 +0100

    no dupe usernames

--- home_page.js
+++ home_page.js
@@ -2,36 +2,43 @@ const registerForm = document.querySelector("#registerForm");
 const loginForm = document.querySelector("#loginForm");
 
 registerForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    checkUsernameExists(e);
+    e.preventDefault()
+    checkUsernameExists(e)
 })
 
 async function checkUsernameExists(e) {
     try {
+        let dupe;
         await fetch(`http://localhost:3000/users`)
         .then(res => res.json())
         .then(data => {
-            let duplicate = 0;
             data.forEach(user => {
                 if (e.target.name.value === user.name) {
-                    duplicate++;
+                    dupe = true;
                 }
             })
-            if (duplicate === 0) {
-                e.preventDefault();
-                const options = {
-                    method : "POST",
-                    headers: { "Content-Type": "application/json"},
-                    body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-                }
-                fetch("http://localhost:3000/users", options);
-            }
-            else {
-                console.log('Username in use.');
-            }
         });
+        if (dupe) {
+            Promise.reject(new Error('Username in use.'));
+        } else {
+            registerAccount(e);
+        }
     }
     catch (err) {
         console.log(err);
     }
+}
+
+async function registerAccount(e) {
+    e.preventDefault();
+    const options = {
+        method : "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+    }
+    await fetch("http://localhost:3000/users", options);
+}
+
+function resetRegistration() {
+    document.querySelector('#registerForm').reset();
 }
