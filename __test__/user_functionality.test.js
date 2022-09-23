40bb15888cb8f28886bf9f70b697eb6e1fc62b69
Littlerou <izzymelvin4@gmail.com>
2022-09-23 10:06:21 +0100

    add test for checklist button

--- __test__/user_functionality.test.js
+++ __test__/user_functionality.test.js
@@ -3,10 +3,12 @@ const renderDOM = require('./helpers')
 let dom;
 let document;
 
+
 describe('user_home_page.html', () =>{
     beforeEach(async () => {
         dom = await renderDOM('static/html/user_home_page.html')
         document = await dom.window.document
+        
     })
 
     it('check getDate() works to get todays date', () =>{
@@ -14,5 +16,12 @@ describe('user_home_page.html', () =>{
         let date = new Date();
         const month = date.toLocaleString('default', { month: 'long' });
         expect(todaysDate.textContent).toContain(`${date.getDate()} ${month} 2022`)
+    })
+
+    it('check get checklist button renders list of items to do', () => {
+        const getChecklist = document.querySelector('#checklistButton')
+        getChecklist.dispatchEvent(new dom.window.Event('click'))
+        const taskForm = document.querySelector('.taskForm')
+        expect(taskForm.hasChildNodes()).toBe(true)
     })
 })
